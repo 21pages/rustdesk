383923b3cf4bfda1a40b6da95535530c4db4857b
21pages <sunboeasy@gmail.com>
2024-09-09 17:49:26 +0800

    multi instance
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -10,60 +10,58 @@
 #include "flutter_window.h"
 #include "utils.h"
 
-typedef char** (*FUNC_RUSTDESK_CORE_MAIN)(int*);
-typedef void (*FUNC_RUSTDESK_FREE_ARGS)( char**, int);
-typedef int (*FUNC_RUSTDESK_GET_APP_NAME)(wchar_t*, int);
+typedef char **(*FUNC_RUSTDESK_CORE_MAIN)(int *);
+typedef void (*FUNC_RUSTDESK_FREE_ARGS)(char **, int);
+typedef int (*FUNC_RUSTDESK_GET_APP_NAME)(wchar_t *, int);
 /// Note: `--server`, `--service` are already handled in [core_main.rs].
 const std::vector<std::string> parameters_white_list = {"--install", "--cm"};
 
-const wchar_t* getWindowClassName();
+const wchar_t *getWindowClassName();
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command)
-{
+                      _In_ wchar_t *command_line, _In_ int show_command) {
   HINSTANCE hInstance = LoadLibraryA("librustdesk.dll");
-  if (!hInstance)
-  {
+  if (!hInstance) {
     std::cout << "Failed to load librustdesk.dll." << std::endl;
     return EXIT_FAILURE;
   }
   FUNC_RUSTDESK_CORE_MAIN rustdesk_core_main =
-      (FUNC_RUSTDESK_CORE_MAIN)GetProcAddress(hInstance, "rustdesk_core_main_args");
-  if (!rustdesk_core_main)
-  {
+      (FUNC_RUSTDESK_CORE_MAIN)GetProcAddress(hInstance,
+                                              "rustdesk_core_main_args");
+  if (!rustdesk_core_main) {
     std::cout << "Failed to get rustdesk_core_main." << std::endl;
     return EXIT_FAILURE;
   }
   FUNC_RUSTDESK_FREE_ARGS free_c_args =
       (FUNC_RUSTDESK_FREE_ARGS)GetProcAddress(hInstance, "free_c_args");
-  if (!free_c_args)
-  {
+  if (!free_c_args) {
     std::cout << "Failed to get free_c_args." << std::endl;
     return EXIT_FAILURE;
   }
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
   // Remove possible trailing whitespace from command line arguments
-  for (auto& argument : command_line_arguments) {
+  for (auto &argument : command_line_arguments) {
     argument.erase(argument.find_last_not_of(" \n\r\t"));
   }
 
   int args_len = 0;
-  char** c_args = rustdesk_core_main(&args_len);
-  if (!c_args)
-  {
+  char **c_args = rustdesk_core_main(&args_len);
+  if (!c_args) {
     std::string args_str = "";
-    for (const auto& argument : command_line_arguments) {
+    for (const auto &argument : command_line_arguments) {
       args_str += (argument + " ");
     }
-    // std::cout << "RustDesk [" << args_str << "], core returns false, exiting without launching Flutter app." << std::endl;
+    // std::cout << "RustDesk [" << args_str << "], core returns false, exiting
+    // without launching Flutter app." << std::endl;
     return EXIT_SUCCESS;
   }
   std::vector<std::string> rust_args(c_args, c_args + args_len);
   free_c_args(c_args, args_len);
 
   std::wstring app_name = L"RustDesk";
-  FUNC_RUSTDESK_GET_APP_NAME get_rustdesk_app_name = (FUNC_RUSTDESK_GET_APP_NAME)GetProcAddress(hInstance, "get_rustdesk_app_name");
+  FUNC_RUSTDESK_GET_APP_NAME get_rustdesk_app_name =
+      (FUNC_RUSTDESK_GET_APP_NAME)GetProcAddress(hInstance,
+                                                 "get_rustdesk_app_name");
   if (get_rustdesk_app_name) {
     wchar_t app_name_buffer[512] = {0};
     if (get_rustdesk_app_name(app_name_buffer, 512) == 0) {
@@ -72,12 +70,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   }
 
   // Uri links dispatch
-  HWND hwnd = ::FindWindowW(getWindowClassName(), app_name.c_str());
+  HWND hwnd = NULL; //::FindWindowW(getWindowClassName(), app_name.c_str());
   if (hwnd != NULL) {
     // Allow multiple flutter instances when being executed by parameters
     // contained in whitelists.
     bool allow_multiple_instances = false;
-    for (auto& whitelist_param : parameters_white_list) {
+    for (auto &whitelist_param : parameters_white_list) {
       allow_multiple_instances =
           allow_multiple_instances ||
           std::find(command_line_arguments.begin(),
@@ -100,8 +98,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
-  {
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
@@ -113,25 +110,27 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // connection manager hide icon from taskbar
   bool is_cm_page = false;
   auto cmParam = std::string("--cm");
-  if (!command_line_arguments.empty() && command_line_arguments.front().compare(0, cmParam.size(), cmParam.c_str()) == 0) {
+  if (!command_line_arguments.empty() &&
+      command_line_arguments.front().compare(0, cmParam.size(),
+                                             cmParam.c_str()) == 0) {
     is_cm_page = true;
   }
-  command_line_arguments.insert(command_line_arguments.end(), rust_args.begin(), rust_args.end());
+  command_line_arguments.insert(command_line_arguments.end(), rust_args.begin(),
+                                rust_args.end());
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(800, 600);
-  if (!window.CreateAndShow(
-          is_cm_page ? app_name + L" - Connection Manager" : app_name, origin,
-          size, !is_cm_page)) {
-      return EXIT_FAILURE;
+  if (!window.CreateAndShow(is_cm_page ? app_name + L" - Connection Manager"
+                                       : app_name,
+                            origin, size, !is_cm_page)) {
+    return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
 
   ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0))
-  {
+  while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
