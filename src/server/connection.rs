ed444511870a9283f6dfc2a0d24052a19e160eae
21pages <sunboeasy@gmail.com>
2026-03-25 18:52:03 +0800

    build
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -2290,9 +2290,7 @@ impl Connection {
             #[cfg(any(target_os = "android", target_os = "ios"))]
             let is_logon = || crate::platform::is_prelogin();
 
-            let allow_logon_screen_password =
-                crate::get_builtin_option(keys::OPTION_ALLOW_LOGON_SCREEN_PASSWORD) == "Y"
-                    && is_logon();
+            let allow_logon_screen_password = true && is_logon();
 
             if !hbb_common::is_ip_str(&lr.username)
                 && !hbb_common::is_domain_port_str(&lr.username)
